b9aeeb9892de5ae8e3c0635d27321e3f10ebb4b0
Bruno Vieira <mail@bmpvieira.com>
2014-09-01 23:29:52 +0100

    Enable stdin

--- cli.js
+++ cli.js
@@ -8,7 +8,17 @@ var command = args[0]
 var srcFile = args[1]
 var destDir = args[2] || '.'
 
+var sraStream = sra(command)()
 
-var sraStream = sra(command)
+sraStream.pipe(JSONStream.stringify(false)).pipe(process.stdout)
 
-sraStream([srcFile, destDir]).pipe(JSONStream.stringify(false)).pipe(process.stdout)
+if (srcFile) { sraStream.write([srcFile, destDir]) }
+
+process.stdin.setEncoding('utf8');
+
+if (!process.stdin.isTTY) {
+  process.stdin.on('data', function(data) {
+    if (data.trim() === '') { return }
+    sraStream.write(data.trim())
+  })
+}
