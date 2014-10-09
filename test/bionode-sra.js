eb9837847d2b96c0c0891dd419a7560e01e348ad
Bruno Vieira <mail@bmpvieira.com>
2014-10-09 16:32:58 +0100

    Switch testing from mocha/should to tape for easier testling support

--- test/bionode-sra.js
+++ test/bionode-sra.js
@@ -2,35 +2,32 @@ var sra = require('../')
 var fs = require('fs')
 var request = require('request')
 var crypto = require('crypto')
-var should = require('should')
-
-require('mocha')
+var test = require('tape')
 
 
-describe("Extract fastq from SRA", function() {
-  this.timeout(600000);
-  it("should take a srcFile and destDir strings and extract a fastq", function(done) {
-    var sraURL = 'http://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR150/SRR1509835/SRR1509835.sra'
-    var downloadSRA = request(sraURL)
-    downloadSRA.pipe(fs.createWriteStream('test/SRR1509835.sra'))
-    downloadSRA.on('complete', dumpFastq)
-    function dumpFastq() {
-      var fastqDump = sra('fastq-dump')
-      var srcFile = 'test/SRR1509835.sra'
-      var destDir = 'test/'
-      fastqDump([srcFile, destDir]).on('data', fastqDumped)
+test("Extract fastq from SRA", function(t) {
+  t.plan(1)
+  var sraURL = 'http://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR150/SRR1509835/SRR1509835.sra'
+  var downloadSRA = request(sraURL)
+  downloadSRA.pipe(fs.createWriteStream('test/SRR1509835.sra'))
+  downloadSRA.on('complete', dumpFastq)
+  function dumpFastq() {
+    var fastqDump = sra('fastq-dump')
+    var srcFile = 'test/SRR1509835.sra'
+    var destDir = 'test/'
+    fastqDump([srcFile, destDir]).on('data', fastqDumped)
+  }
+  function fastqDumped(data) {
+    var sha256sum = crypto.createHash('sha256');
+    var fastqPath = data.srcFile.replace('sra', 'fastq')
+    var fastqFile = fs.createReadStream(fastqPath);
+    fastqFile.on('data', function(d) { sha256sum.update(d) })
+    fastqFile.on('end', checkSum)
+    function checkSum() {
+      var sha256 = sha256sum.digest('hex')
+      var hash = 'f13dd1b06542a0a5e2ce078f8ab123abfbd70fe49400cae66f430a2d980e42d9'
+      var msg = "should take a srcFile and destDir strings and extract a fastq"
+      t.equal(sha256, hash, msg)
     }
-    function fastqDumped(data) {
-      var sha256sum = crypto.createHash('sha256');
-      var fastqPath = data.srcFile.replace('sra', 'fastq')
-      var fastqFile = fs.createReadStream(fastqPath);
-      fastqFile.on('data', function(d) { sha256sum.update(d) })
-      fastqFile.on('end', checkSum)
-      function checkSum() {
-        var sha256 = sha256sum.digest('hex')
-        sha256.should.equal('f13dd1b06542a0a5e2ce078f8ab123abfbd70fe49400cae66f430a2d980e42d9')
-        done()
-      }
-    }
-  })
+  }
 })
