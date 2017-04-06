3591cff73622bb202f15a3be7ed9f95806d14447
Amanjeev Sethi <aj@amanjeev.com>
2017-04-06 06:02:17 -0400

    PR #5 fixed formatting, based on standard

--- test/bionode-sra.js
+++ test/bionode-sra.js
@@ -4,29 +4,28 @@ var request = require('request')
 var crypto = require('crypto')
 var test = require('tape')
 
-
-test("Extract fastq from SRA", function(t) {
+test('Extract fastq from SRA', function (t) {
   t.plan(1)
   var sraURL = 'http://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR150/SRR1509835/SRR1509835.sra'
   var downloadSRA = request(sraURL)
   downloadSRA.pipe(fs.createWriteStream('test/SRR1509835.sra'))
   downloadSRA.on('complete', dumpFastq)
-  function dumpFastq() {
+  function dumpFastq () {
     var fastqDump = sra('fastq-dump')
     var srcFile = 'test/SRR1509835.sra'
     var destDir = 'test/'
     fastqDump([srcFile, destDir]).on('data', fastqDumped)
   }
-  function fastqDumped(data) {
-    var sha256sum = crypto.createHash('sha256');
+  function fastqDumped (data) {
+    var sha256sum = crypto.createHash('sha256')
     var fastqPath = data.srcFile.replace('sra', 'fastq')
-    var fastqFile = fs.createReadStream(fastqPath);
-    fastqFile.on('data', function(d) { sha256sum.update(d) })
+    var fastqFile = fs.createReadStream(fastqPath)
+    fastqFile.on('data', function (d) { sha256sum.update(d) })
     fastqFile.on('end', checkSum)
-    function checkSum() {
+    function checkSum () {
       var sha256 = sha256sum.digest('hex')
       var hash = 'f13dd1b06542a0a5e2ce078f8ab123abfbd70fe49400cae66f430a2d980e42d9'
-      var msg = "should take a srcFile and destDir strings and extract a fastq"
+      var msg = 'should take a srcFile and destDir strings and extract a fastq'
       t.equal(sha256, hash, msg)
     }
   }
